575a46e2d856106633c4ce51d12a76962751a9e0
Joe Yates <joe.g.yates@gmail.com>
2011-10-09 22:36:55 +0100

    Added eunit tests for private functions

--- binary_tree/binary_tree.erl
+++ binary_tree/binary_tree.erl
@@ -1,8 +1,9 @@
 -module(binary_tree).
 
--include("node.hrl").
-
 -export([bfs/1, dfs/1, from_bfs_dfs/2]).
+
+-include("node.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 bfs(Node) ->
   lists:flatten(internal_bfs(Node)).
@@ -68,3 +69,43 @@ zip_lists_of_lists(LL1, LL2) ->
 	       [L1, L2] -> L1 ++ L2
 	   end,
     [Head] ++ zip_lists_of_lists(Rest1, Rest2).
+
+%% Tests for private functions
+
+spliton_test_() ->
+    [?_assert(spliton(2, [1, 2, 3]) =:= {[1],   [3]}),
+     ?_assert(spliton(1, [1, 2, 3]) =:= {[],    [2, 3]}),
+     ?_assert(spliton(3, [1, 2, 3]) =:= {[1,2], []}),
+     ?_assert(spliton(4, [1, 2, 3]) =:= not_found)].
+
+internal_bfs_test_() ->
+    Root = #node{value = 1},
+    Left = #node{value = 1, left = #node{value = 2}},
+    Right = #node{value = 1, right = #node{value = 3}},
+    Both = #node{value = 1,
+		 left = #node{value = 2},
+		 right = #node{value = 3}},
+    Large = #node{value = 1,
+                  left = #node{value = 2,
+			       left = #node{value = 7,
+					    left = #node{value = 4},
+					    right = #node{value = 5}},
+			       right = #node{value = 3}},
+                  right = #node{value = 6,
+				left = #node{value = 8}}},
+    [?_assert(internal_bfs(Root)  =:= [[1]]),
+     ?_assert(internal_bfs(Left)  =:= [[1], [2]]),
+     ?_assert(internal_bfs(Right) =:= [[1], [3]]),
+     ?_assert(internal_bfs(Both)  =:= [[1], [2, 3]]),
+     ?_assert(internal_bfs(Large) =:= [[1],[2,6],[7,3,8],[4,5]])].
+
+zip_lists_of_lists_test_() ->
+    [?_assert(zip_lists_of_lists([],            [])            =:= []),
+     ?_assert(zip_lists_of_lists([[1]],         [])            =:= [[1]]),
+     ?_assert(zip_lists_of_lists([[1, 2]],      [])            =:= [[1, 2]]),
+     ?_assert(zip_lists_of_lists([[1], [2]],    [])            =:= [[1], [2]]),
+     ?_assert(zip_lists_of_lists([[1, 2], [3]], [])            =:= [[1, 2], [3]]),
+     ?_assert(zip_lists_of_lists([],            [[1]])         =:= [[1]]),
+     ?_assert(zip_lists_of_lists([],            [[1, 2]])      =:= [[1, 2]]),
+     ?_assert(zip_lists_of_lists([],            [[1], [2]])    =:= [[1], [2]]),
+     ?_assert(zip_lists_of_lists([],            [[1, 2], [3]]) =:= [[1, 2], [3]])].
