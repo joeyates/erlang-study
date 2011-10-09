94d4d0d1940ba966ef7920a0a46cf209bb540b55
Joe Yates <joe.g.yates@gmail.com>
2011-10-09 21:09:42 +0100

    Added bfs and dfs methods

--- binary_tree/binary_tree.erl
+++ binary_tree/binary_tree.erl
@@ -2,10 +2,21 @@
 
 -include("node.hrl").
 
--export([from_bfs_dfs/2]).
+-export([bfs/1, dfs/1, from_bfs_dfs/2]).
+
+bfs(Node) ->
+  lists:flatten(internal_bfs(Node)).
+
+dfs({node, Value, undefined, undefined}) ->
+    [Value];
+dfs({node, Value, Left, undefined}) ->
+    dfs(Left) ++ [Value];
+dfs({node, Value, undefined, Right}) ->
+    [Value] ++ dfs(Right);
+dfs({node, Value, Left, Right}) ->
+    dfs(Left) ++ [Value] ++ dfs(Right).
 
 from_bfs_dfs(_, []) ->
-    % Done
     undefined;
 from_bfs_dfs(Bfs, Dfs) ->
     [BHead | BTail] = Bfs,
@@ -27,3 +38,30 @@ spliton(Elem, List) ->
 	_Else ->
 	    not_found
     end.
+
+% create lists of nodes at a certain depth
+internal_bfs({node, Value, undefined, undefined}) ->
+    [[Value]];
+internal_bfs({node, Value, Left, undefined}) ->
+    [[Value]] ++ internal_bfs(Left);
+internal_bfs({node, Value, undefined, Right}) ->
+    [[Value]] ++ internal_bfs(Right);
+internal_bfs({node, Value, Left, Right}) ->
+    LLLeft = internal_bfs(Left),
+    LLRight = internal_bfs(Right),
+    [[Value]] ++ zip_lists_of_lists(LLLeft, LLRight).
+
+% zip unequal length lists of lists
+zip_lists_of_lists(LL1, []) ->
+    LL1;
+zip_lists_of_lists([], LL2) ->
+    LL2;
+zip_lists_of_lists(LL1, LL2) ->
+    [Head1 | Rest1] = LL1,
+    [Head2 | Rest2] = LL2,
+    Head = case [Head1, Head2] of
+	       [L1, []] -> L1;
+	       [[], L2] -> L2;
+	       [L1, L2] -> L1 ++ L2
+	   end,
+    [Head] ++ zip_lists_of_lists(Rest1, Rest2).
