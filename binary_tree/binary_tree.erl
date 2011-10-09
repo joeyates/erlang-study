b7881719d50f635ca92313107473014c10e6027f
Joe Yates <joe.g.yates@gmail.com>
2011-10-09 16:49:01 +0100

    Whitespace

--- binary_tree/binary_tree.erl
+++ binary_tree/binary_tree.erl
@@ -4,26 +4,26 @@
 
 -compile(export_all).
 
-from_bfs_dfs(_,[]) ->
+from_bfs_dfs(_, []) ->
     % Done
     undefined;
-from_bfs_dfs(Bfs,Dfs) ->
-    [BHead|BTail] = Bfs,
-    case spliton(BHead,Dfs) of
-	{Left,Right} ->
-	    #node{value=BHead,
-		  left=from_bfs_dfs(BTail,Left),
-		  right=from_bfs_dfs(BTail,Right)};
+from_bfs_dfs(Bfs, Dfs) ->
+    [BHead | BTail] = Bfs,
+    case spliton(BHead, Dfs) of
+	{Left, Right} ->
+	    #node{value = BHead,
+		  left = from_bfs_dfs(BTail, Left),
+		  right = from_bfs_dfs(BTail, Right)};
 	not_found ->
 	    %% Try the next Dfs value
-	    from_bfs_dfs(BTail,Dfs)
+	    from_bfs_dfs(BTail, Dfs)
     end.
 
-spliton(Elem,List) ->
-    {Before,From} = lists:splitwith(fun(A) -> A /= Elem end, List),
+spliton(Elem, List) ->
+    {Before, From} = lists:splitwith(fun(A) -> A /= Elem end, List),
     case From of
-	[Elem|After] ->
-	    {Before,After};
+	[Elem | After] ->
+	    {Before, After};
 	_Else ->
 	    not_found
     end.
