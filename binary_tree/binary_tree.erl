4f8fd0490fcda1f71bb92ec7180e1bef18ad4880
Joe Yates <joe.g.yates@gmail.com>
2013-04-01 12:29:07 +0200

    Whitespace

--- binary_tree/binary_tree.erl
+++ binary_tree/binary_tree.erl
@@ -9,69 +9,69 @@ bfs(Node) ->
   lists:flatten(internal_bfs(Node)).
 
 dfs({node, Value, undefined, undefined}) ->
-    [Value];
+  [Value];
 dfs({node, Value, Left, undefined}) ->
-    dfs(Left) ++ [Value];
+  dfs(Left) ++ [Value];
 dfs({node, Value, undefined, Right}) ->
-    [Value] ++ dfs(Right);
+  [Value] ++ dfs(Right);
 dfs({node, Value, Left, Right}) ->
-    dfs(Left) ++ [Value] ++ dfs(Right).
+  dfs(Left) ++ [Value] ++ dfs(Right).
 
 from_bfs_dfs(_, []) ->
-    undefined;
+  undefined;
 from_bfs_dfs([], _) ->
-    undefined;
+  undefined;
 from_bfs_dfs(Bfs, Dfs) ->
-    [BHead | BTail] = Bfs,
-    case spliton(BHead, Dfs) of
-	{Left, Right} ->
-	    #node{value = BHead,
-		  left = from_bfs_dfs(BTail, Left),
-		  right = from_bfs_dfs(BTail, Right)};
+  [BHead | BTail] = Bfs,
+  case spliton(BHead, Dfs) of
+  {Left, Right} ->
+    #node{value = BHead,
+	  left = from_bfs_dfs(BTail, Left),
+	  right = from_bfs_dfs(BTail, Right)};
 	not_found ->
-	    %% Try the next Bfs value
-	    from_bfs_dfs(BTail, Dfs)
-    end.
+    %% Try the next Bfs value
+    from_bfs_dfs(BTail, Dfs)
+  end.
 
 % return two lists:
 % - elements before the first occurence of Elem,
 % - elements after the first occurence of Elem.
 spliton(Elem, List) ->
-    {Before, From} = lists:splitwith(fun(A) -> A /= Elem end, List),
-    case From of
-	% If Elem is in List, it will be the first element in From
-	[Elem | After] ->
-	    {Before, After};
+  {Before, From} = lists:splitwith(fun(A) -> A /= Elem end, List),
+  case From of
+  % If Elem is in List, it will be the first element in From
+  [Elem | After] ->
+    {Before, After};
 	_ ->
 	    not_found
-    end.
+  end.
 
 % create lists of lists of elements at a certain depth
 internal_bfs({node, Value, undefined, undefined}) ->
-    [[Value]];
+  [[Value]];
 internal_bfs({node, Value, Left, undefined}) ->
-    [[Value]] ++ internal_bfs(Left);
+  [[Value]] ++ internal_bfs(Left);
 internal_bfs({node, Value, undefined, Right}) ->
-    [[Value]] ++ internal_bfs(Right);
+  [[Value]] ++ internal_bfs(Right);
 internal_bfs({node, Value, Left, Right}) ->
-    LLLeft = internal_bfs(Left),
-    LLRight = internal_bfs(Right),
-    [[Value]] ++ zip_lists_of_lists(LLLeft, LLRight).
+  LLLeft = internal_bfs(Left),
+  LLRight = internal_bfs(Right),
+  [[Value]] ++ zip_lists_of_lists(LLLeft, LLRight).
 
 % zip unequal length lists of lists
 zip_lists_of_lists(LL1, []) ->
-    LL1;
+  LL1;
 zip_lists_of_lists([], LL2) ->
-    LL2;
+  LL2;
 zip_lists_of_lists(LL1, LL2) ->
-    [Head1 | Rest1] = LL1,
-    [Head2 | Rest2] = LL2,
-    Head = case [Head1, Head2] of
-	       [L1, []] -> L1;
-	       [[], L2] -> L2;
-	       [L1, L2] -> L1 ++ L2
-	   end,
-    [Head] ++ zip_lists_of_lists(Rest1, Rest2).
+  [Head1 | Rest1] = LL1,
+  [Head2 | Rest2] = LL2,
+  Head = case [Head1, Head2] of
+    [L1, []] -> L1;
+      [[], L2] -> L2;
+      [L1, L2] -> L1 ++ L2
+  end,
+  [Head] ++ zip_lists_of_lists(Rest1, Rest2).
 
 %% Tests for private functions
 
